90cf20a4386ca79db87effede1a3bc406106c334
bryam <bryam.vasquez@tektonlabs.com>
2019-06-27 16:40:42 -0500

    task view

---
+++ iRumi/ViewController/Owner/OwnerTasksViewController.swift
@@ -0,0 +1,32 @@
+//
+//  OwnerTasksViewController.swift
+//  iRumi
+//
+//  Created by Bryam  on 6/27/19.
+//  Copyright © 2019 Israel Matias. All rights reserved.
+//
+
+import UIKit
+
+class OwnerTasksViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tasksTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tasksTableView.dataSource = self
+    }
+
+}
+
+extension OwnerTasksViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
+        return cell
+    }
+}
